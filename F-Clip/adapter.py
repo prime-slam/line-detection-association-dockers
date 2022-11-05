62ba86b2d1882966063622dc99384c745248b448
Kirill Ivanov <ivanov.kirill.a@mail.ru>
2022-11-05 22:10:48 +0300

    License updated

--- F-Clip/adapter.py
+++ F-Clip/adapter.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, Kirill Ivanov, Anastasiia Kornilova and Dmitry Iarosh
+# Copyright (c) 2022, Kirill Ivanov, Anastasiia Kornilova and Dmitrii Iarosh
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
