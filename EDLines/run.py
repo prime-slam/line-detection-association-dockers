d227d76ffdb234c1601adc10991e7221cb2cc84a
kirill-ivanov-a <ivanov.kirill.a@mail.ru>
2022-11-23 15:51:08 +0300

    EDLines Docker Image
    - EDLines Docker Image added

---
+++ EDLines/run.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2022, Kirill Ivanov, Anastasiia Kornilova and Dmitrii Iarosh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pathlib import Path
+
+from adapter import Adapter
+from common.parser import create_base_parser
+
+if __name__ == "__main__":
+    parser = create_base_parser()
+
+    args = parser.parse_args()
+    Adapter(
+        image_path=Path(args.imgs),
+        output_path=Path(args.output),
+        lines_output_directory=Path(args.lines_dir),
+        scores_output_directory=Path(args.scores_dir),
+    ).run()
